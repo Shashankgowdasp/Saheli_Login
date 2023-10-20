6d13d4a8a00551bc0c6b651be0d9d00ad7371fe6
Shashankgowdasp <shashankspgowda@gmail.com>
2023-10-20 10:39:24 +0000

    Code Synced by DhiWise

---
+++ lib/core/utils/image_constant.dart
@@ -0,0 +1,32 @@
+class ImageConstant {
+  // Image folder path
+  static String imagePath = 'assets/images';
+
+  // MainActivity images
+  static String imgMenu = '$imagePath/img_menu.svg';
+
+  static String imgOverflowmenu = '$imagePath/img_overflowmenu.svg';
+
+  static String imgImage6 = '$imagePath/img_image6.png';
+
+  static String imgArrowright = '$imagePath/img_arrowright.svg';
+
+  static String imgImage5 = '$imagePath/img_image5.png';
+
+  static String imgImage11 = '$imagePath/img_image11.png';
+
+  static String imgImage9 = '$imagePath/img_image9.png';
+
+  static String imgImage10 = '$imagePath/img_image10.png';
+
+  static String imgImage656x56 = '$imagePath/img_image6_56x56.png';
+
+  static String imgImage8 = '$imagePath/img_image8.png';
+
+  static String imgImage7 = '$imagePath/img_image7.png';
+
+  // Common images
+  static String imgArrowdropdown = '$imagePath/img_arrowdropdown.svg';
+
+  static String imageNotFound = 'assets/images/image_not_found.png';
+}
---
+++ lib/theme/app_decoration.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:shashankgowdasp_s_application2/core/app_export.dart';
+
+class AppDecoration {
+  // Background decorations
+  static BoxDecoration get background => BoxDecoration(
+        color: appTheme.blueGray50,
+      );
+
+  // Fill decorations
+  static BoxDecoration get fillGreen => BoxDecoration(
+        color: appTheme.green50,
+      );
+  static BoxDecoration get fillWhiteA => BoxDecoration(
+        color: appTheme.whiteA700,
+      );
+
+  // Outline decorations
+  static BoxDecoration get outlineDeepOrangeA => BoxDecoration(
+        color: appTheme.red50,
+        border: Border.all(
+          color: appTheme.deepOrangeA400,
+          width: 1.h,
+        ),
+      );
+  static BoxDecoration get outlineGray => BoxDecoration(
+        color: appTheme.whiteA700,
+        boxShadow: [
+          BoxShadow(
+            color: appTheme.gray300,
+            spreadRadius: 2.h,
+            blurRadius: 2.h,
+            offset: Offset(
+              0,
+              2,
+            ),
+          ),
+        ],
+      );
+}
+
+class BorderRadiusStyle {
+  // Rounded borders
+  static BorderRadius get roundedBorder4 => BorderRadius.circular(
+        4.h,
+      );
+  static BorderRadius get roundedBorder8 => BorderRadius.circular(
+        8.h,
+      );
+}
+
+// Comment/Uncomment the below code based on your Flutter SDK version.
+
+// For Flutter SDK Version 3.7.2 or greater.
+
+double get strokeAlignInside => BorderSide.strokeAlignInside;
+
+double get strokeAlignCenter => BorderSide.strokeAlignCenter;
+
+double get strokeAlignOutside => BorderSide.strokeAlignOutside;
+
+// For Flutter SDK Version 3.7.1 or less.
+
+// StrokeAlign get strokeAlignInside => StrokeAlign.inside;
+//
+// StrokeAlign get strokeAlignCenter => StrokeAlign.center;
+//
+// StrokeAlign get strokeAlignOutside => StrokeAlign.outside;
